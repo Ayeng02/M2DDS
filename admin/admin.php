0d0333c20551c870216be3754d1ea8f704250af0
YaToYaMaM <froilancaete19@gmail.com>
2024-09-02 22:48:31 +0800

    admin part
    new added

---
+++ admin/admin.php
@@ -0,0 +1,36 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meat To Door Delivery System Admin</title>
+    <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <div class="whole">
+
+        <div class="sidebar">
+            <div class="dashboard-block">
+                <img class="dashboard-img" src="dashboard.png" alt="">
+                <h3>Dashboard</h3>
+            </div>
+            <button id="prod">PRODUCT</button><br>
+            <input type="button" name="product" id="prod" value="USER"><br>
+            <input type="button" name="product" id="prod" value="SALES"><br>
+           </div>
+           
+     
+    
+        <div class="header">
+
+        <h1>Meat To Door Delivery System</h1>
+        </div>    
+</div>
+</body>
+</html>
