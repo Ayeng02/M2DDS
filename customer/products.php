e577f0e58a29ce7fdf480c4092c64bb6a279fc12
Ayeng02 <arieldohinog@gmail.com>
2024-09-02 20:50:26 +0800

    Added updated fontawesome

--- customer/products.php
+++ customer/products.php
@@ -57,6 +57,7 @@ $products = $stmt->get_result();
     <title>Products - Meat-to-Door Delivery</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
     <link rel="stylesheet" href="../css/home.css">
     <style>
         body {
