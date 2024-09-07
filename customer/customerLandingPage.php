18aa520bcb22a764179f42900ed7c459b41d9758
Ayeng02 <arieldohinog@gmail.com>
2024-09-07 16:43:35 +0800

    g-0

--- customer/customerLandingPage.php
+++ customer/customerLandingPage.php
@@ -265,7 +265,7 @@
         <!-- Product Cards for what's New-->
         <h3 class="mb-4" style="text-align: center; padding-top: 20px; font-weight: bold; color: crimson;">What's New</h3>
         <hr class="under">
-        <div class="row g-2">
+        <div class="row g-0">
             <?php
             include '../includes/db_connect.php';
 
