16a6018bdeb7f3bca04980c306e5cdbdeff4d503
Ayeng02 <arieldohinog@gmail.com>
2024-09-16 19:38:46 +0800

    huhu

--- ordr_manager/order_manager.php
+++ ordr_manager/order_manager.php
@@ -844,7 +844,6 @@ label{
     <script src="https://cdn.datatables.net/2.1.5/js/dataTables.js"></script>
     <script src="https://cdn.datatables.net/2.1.5/js/dataTables.bootstrap5.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../js/order_manager.js"></script>
     <!-- Include DataTables JS -->
 
     <script>
