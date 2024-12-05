ecd0660a99eb965dd1b2c27d25f9268a0f5fab3d
Ayeng02 <arieldohinog@gmail.com>
2024-12-06 03:27:09 +0800

    try

--- ordr_manager/order_manager.php
+++ ordr_manager/order_manager.php
@@ -67,7 +67,7 @@ $processingQuery = "
     JOIN product_tbl p ON o.prod_code = p.prod_code
     JOIN brgy_tbl b ON o.order_barangay = b.Brgy_name
     WHERE o.status_code = 2 
-      AND DATE(o.order_date) = CURDATE()
+      AND DATE(o.order_date) = DATE(DATE_ADD(NOW(), INTERVAL 5 HOUR))
     ORDER BY o.order_date DESC";
 
 $processingResult = $conn->query($processingQuery);
